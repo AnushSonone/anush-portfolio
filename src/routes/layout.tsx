a56bccf34c4bfde17f19a700d0ff86d20d25346f
lmaosoggypancakes <macerajosef@gmail.com>
2023-10-27 02:38:36 -0400

    chore: build errors

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -14,7 +14,7 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export default component$(async () => {
+export default component$(() => {
   return (
     <>
       <Navbar />
