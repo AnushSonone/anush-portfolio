5da348f962e76c122c2d5d1a97437df7471c1af5
Anush <bucketlid4@gmail.com>
2024-05-06 13:36:55 -0500

    more updates to articles

--- src/routes/articles/data.ts
+++ src/routes/articles/data.ts
@@ -11,7 +11,7 @@ export const blogs: BlogMeta[] = [
   {
     title: "My College Application Journey!",
     date: "5/5/24",
-    description: "How I got into UT Austin CS and my 14 other rejections.",
+    description: "How I got into UT Austin CS and my 11 other rejections.",
     id: "college-application-journey",
     image: "/blogs/college-application-journey.png",
   },
